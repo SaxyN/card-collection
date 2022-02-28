c005f9d11c0980f939214eadc8db125b21f4f9d2
Jon M <jcmeisner7@gmail.com>
2022-02-28 09:12:49 -0800

    Removed Test Card

--- src/components/Inventory/InventoryFrame.js
+++ src/components/Inventory/InventoryFrame.js
@@ -56,7 +56,6 @@ const InventoryFrame = ({ inventory }) => {
                     <HoloCard />
                     <ShatterCard />
                     <LegendaryCard />
-                    <TestCard />
                 </>
             }
         </Card>
