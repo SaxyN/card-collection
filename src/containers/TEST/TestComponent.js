c542a1e47625922ec7d974747e05d1094458ad2e
Jon M <jcmeisner7@gmail.com>
2022-02-28 19:42:05 -0800

    This is going to be scrapped very soon

--- src/containers/TEST/TestComponent.js
+++ src/containers/TEST/TestComponent.js
@@ -1,75 +1,71 @@
 import "./styles.css";
+import "./runner.js";
 
 const TestComponent = () => {
 
-    // ===========================================================
-// See tutorial at : 
-// https://css-tricks.com/animate-a-container-on-mouse-over-using-perspective-and-transform/
-// ===========================================================
-
-(function() {
+  (function () {
     // Init
     var container = document.getElementById("container"),
-        inner = document.getElementById("inner");
-  
+      inner = document.getElementById("inner");
+
     // Mouse
     var mouse = {
       _x: 0,
       _y: 0,
       x: 0,
       y: 0,
-      updatePosition: function(event) {
+      updatePosition: function (event) {
         var e = event || window.event;
         this.x = e.clientX - this._x;
         this.y = (e.clientY - this._y) * -1;
       },
-      setOrigin: function(e) {
+      setOrigin: function (e) {
         this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
         this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
       },
-      show: function() {
+      show: function () {
         return "(" + this.x + ", " + this.y + ")";
       }
     };
-  
+
     // Track the mouse position relative to the center of the container.
     mouse.setOrigin(container);
-  
+
     //----------------------------------------------------
-  
+
     var counter = 0;
     var refreshRate = 10;
-    var isTimeToUpdate = function() {
+    var isTimeToUpdate = function () {
       return counter++ % refreshRate === 0;
     };
-  
+
     //----------------------------------------------------
-  
-    var onMouseEnterHandler = function(event) {
+
+    var onMouseEnterHandler = function (event) {
       update(event);
     };
-  
-    var onMouseLeaveHandler = function() {
+
+    var onMouseLeaveHandler = function () {
       inner.style = "";
     };
-  
-    var onMouseMoveHandler = function(event) {
+
+    var onMouseMoveHandler = function (event) {
       if (isTimeToUpdate()) {
         update(event);
       }
     };
-  
+
     //----------------------------------------------------
-  
-    var update = function(event) {
+
+    var update = function (event) {
       mouse.updatePosition(event);
       updateTransformStyle(
         (mouse.y / inner.offsetHeight / 2).toFixed(2),
         (mouse.x / inner.offsetWidth / 2).toFixed(2)
       );
     };
-  
-    var updateTransformStyle = function(x, y) {
+
+    var updateTransformStyle = function (x, y) {
       var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
       inner.style.transform = style;
       inner.style.webkitTransform = style;
@@ -77,24 +73,25 @@ const TestComponent = () => {
       inner.style.msTransform = style;
       inner.style.oTransform = style;
     };
-  
+
     //--------------------------------------------------------
-  
+
     container.onmousemove = onMouseMoveHandler;
     container.onmouseleave = onMouseLeaveHandler;
     container.onmouseenter = onMouseEnterHandler;
   })();
-  
-    return (
-<div id="container">
-  <div id="inner">
-    <div class="demo-img">
-      <div class="demo-overlay">
+  return (
+    <div className="outer">
+      <div id="container">
+        <div id="inner">
+          <div class="demo-img">
+            <div class="demo-overlay">
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-  </div>
-    )
+  )
 }
 
 export default TestComponent;
