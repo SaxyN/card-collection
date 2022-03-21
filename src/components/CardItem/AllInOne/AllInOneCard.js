7adce28409d23f931550798c59d7d85f19be17a5
Jon M <jcmeisner7@gmail.com>
2022-03-21 09:18:28 -0700

    Start Of Combining all Cards under one component

---
+++ src/components/CardItem/AllInOne/AllInOneCard.js
@@ -0,0 +1,12 @@
+import "./master_style.scss";
+
+const AllInOneCard = ({ test }) => {
+    switch (test) {
+        case "card_default":
+            return ""
+        default:
+            return ""
+    }
+}
+
+export default AllInOneCard;
