a8cf52038a2ebb60ca75b3a7335faca8fcbc863c
Jon M <jcmeisner7@gmail.com>
2022-03-21 09:18:07 -0700

    Implemented size tagging for cards

--- src/components/CardHandler/CardHandler.js
+++ src/components/CardHandler/CardHandler.js
@@ -3,17 +3,17 @@ import HoloCard from "../CardItem/HoloCard/HoloCard";
 import ShatterCard from "../CardItem/ShatterCard/ShatterCard";
 import LegendaryCard from "../CardItem/LegendaryCard/LegendaryCard";
 
-const CardHandler = ({ cardImage, cardType }) => {
+const CardHandler = ({ cardImage, cardType, sizeTag }) => {
 
     switch (cardType) {
         case 0:
-            return <NormalCard imageFace={cardImage} imageStyle={{ width: "262.5px", height: "368.75px", borderRadius: "5% / 3.5%" }} />
+            return <NormalCard imageFace={cardImage} imageStyle={{ width: "262.5px", height: "368.75px", borderRadius: "5% / 3.5%" }} sizeTag={sizeTag} />
         case 1:
-            return <HoloCard imageFace={cardImage} imageStyle={{ width: "262.5px", height: "368.75px", borderRadius: "5% / 3.5%" }} />
+            return <HoloCard imageFace={cardImage} imageStyle={{ width: "262.5px", height: "368.75px", borderRadius: "5% / 3.5%" }} sizeTag={sizeTag} />
         case 2:
-            return <ShatterCard imageFace={cardImage} imageStyle={{ width: "262.5px", height: "368.75px", borderRadius: "5% / 3.5%" }} />
+            return <ShatterCard imageFace={cardImage} imageStyle={{ width: "262.5px", height: "368.75px", borderRadius: "5% / 3.5%" }} sizeTag={sizeTag} />
         case 3:
-            return <LegendaryCard imageFace={cardImage} imageStyle={{ width: "262.5px", height: "368.75px", borderRadius: "5% / 3.5%" }} />
+            return <LegendaryCard imageFace={cardImage} imageStyle={{ width: "262.5px", height: "368.75px", borderRadius: "5% / 3.5%" }} sizeTag={sizeTag} />
         default:
             return <>Unknown Card Type</>
     }
